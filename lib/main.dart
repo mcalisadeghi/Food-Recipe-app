3877d53c3831861c3fb25aefea39f260bf726c4c
mcalisadeghi <ali.ke970@gmail.com>
2023-12-24 18:33:31 +0330

    build MainScreen

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Food Recipe',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // colorScheme: ColorScheme.fromSeed(
+          // seedColor: kPrimaryButton,
+        // ),
+        // scaffoldBackgroundColor: kBackMouseButton,
         useMaterial3: true,
       ),
       home: const MainScreen(),
--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int selectedIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        height: 70.0,
+        child: Row(
+          children: [
+            GestureDetector(
+              child:  Column(
+                children: [
+                  Icon(
+                    Icons.home,
+                    color:  selectedIndex == 0 ? Colors.red : Colors.grey,
+                  ),
+                  Text(
+                    'Home',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: selectedIndex == 0 ? Colors.red : Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            GestureDetector(
+              child: Column(
+                children: [
+                  Icon(
+                    Icons.heart_broken,
+                    color: selectedIndex == 1 ? Colors.red : Colors.grey,
+                  ),
+                  Text(
+                    'Favorite',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: selectedIndex == 1 ? Colors.red : Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            GestureDetector(
+              child:  Column(
+                children: [
+                  Icon(
+                    Icons.calculate,
+                    color: selectedIndex == 2 ? Colors.red : Colors.grey,
+                  ),  Text(
+                    'Meal Plan',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: selectedIndex == 2 ? Colors.red : Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
