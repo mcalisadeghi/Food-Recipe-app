f75ce3cc9010cdc9f547b01cf8fad505947f31cc
mcalisadeghi <ali.ke970@gmail.com>
2023-12-25 13:26:20 +0330

    build home_search_bar widget

--- lib/widgets/home_search_bar.dart
+++ lib/widgets/home_search_bar.dart
@@ -1,37 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/home_app_bar.dart';
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                HomeAppBar(),
-                SizedBox(
-                  height: 20.0,
-                ),
-                HomeSearchBar(),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 class HomeSearchBar extends StatelessWidget {
   const HomeSearchBar({
@@ -63,7 +30,8 @@ class HomeSearchBar extends StatelessWidget {
           Expanded(
             child: TextField(
               decoration: InputDecoration(
-                border: InputBorder.none,hintText: 'Search any recipes',
+                border: InputBorder.none,
+                hintText: 'Search any recipes',
                 hintStyle: TextStyle(
                   color: Colors.grey,
                 ),
